7b2dde43af3737b84b363c589d237bf75bb782cc
PranavSindhanuru <pranavsindhanuru@gmail.com>
2021-11-11 21:13:15 +0530

    experimenting on upload

--- main.py
+++ main.py
@@ -1,17 +1,30 @@
 # This is our main python file !
 # we code all the MAIN stuff in this
+from fastapi import FastAPI
+from fastapi.responses import FileResponse
+import uvicorn
+from fastapi import FastAPI, File, UploadFile
 
-# !!!!!!!!!!!  DONT RUN THIS ↓ !!!!!!!!!!!!!!!
-# from fastapi import FastAPI
-# from fastapi.responses import FileResponse
-# import os
-# # import webbrowser
+from PyPDF2 import PdfFileWriter, PdfFileReader
 
-# app = FastAPI()
 
-# @app.get("/")
-# def file():
-#     return FileResponse('dummy.pdf')
+app = FastAPI()
 
-# os.popen('uvicorn main:app --reload')
-# # webbrowser.open('http://127.0.0.1:8000/')
+
+@app.post("/uploadfile/")
+async def create_upload_file(file: UploadFile = File(...)):
+    output = PdfFileWriter()
+    input1 = PdfFileReader(file.file)
+
+    print(f'{input1.getNumPages()}')
+
+    output.addPage(input1.getPage(0))
+    output.addPage(input1.getPage(1).rotateClockwise(90))
+
+    with open("result.pdf", "wb") as f:
+        output.write(f)
+    
+    return FileResponse('result.pdf', headers={'content-disposition': 'attachment; filename=blah.pdf'})
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
